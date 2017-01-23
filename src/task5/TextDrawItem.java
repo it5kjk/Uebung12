f369558e39f58104200b2420f268b62bf8d4cd39
it5kjk <jnkiepert@gmail.com>
2017-01-23 09:15:22 +0100

    add constructor to TextDrawItem class

--- src/task5/TextDrawItem.java
+++ src/task5/TextDrawItem.java
@@ -2,12 +2,28 @@ package task5;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class TextDrawItem {
 	protected Font font;
 	protected Color color;
+	protected String text;
+	protected int x;
+	protected int y;
 	
-	//TODO: implement draw method
+	public TextDrawItem(String s, Font f, Color c, int x, int y) {
+		text = s;
+		font = f;
+		color = c;
+		this.x = x;
+		this.y = y;
+	}
+	
+	void paint(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.drawString(text, x, y);
+	}
 	
 	public Font getFont() {
 		return font;
